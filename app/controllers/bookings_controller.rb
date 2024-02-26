2f8f33deaf13bd69c02c30ecde3a0f1c6bb31288
cy porteous <cy.porteous@pm.me>
2024-02-26 23:42:32 +0000

    a hard day spent adding methods to the bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,36 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = current_user.bookings
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    @game = @booking.game
+  end
+
+  def new
+    @booking = Booking.new
+    @game = Game.find(params[:game_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @game = Game.find(params[:game_id])
+    @booking.game = @game
+
+    if @booking.save
+      redirect_to @booking, notice: 'retro-rent-booking was successful!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+      params.require(:booking).permit(:start_date, :end_date)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :games, only: [:index]
+  resources :games, only: [:index] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
