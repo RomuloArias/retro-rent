af0c1279b92e95f7450a85b17a631dd7352e936b
cy porteous <cy.porteous@pm.me>
2024-02-27 14:25:19 +0000

    games show page bug resolved

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   GENRES = %w[Action Adventure Arcade Combat Fantasy Platform Puzzle Strategy]
   CONSOLES = ["Atari", "Dreamcast", "Game Boy", "Game Boy Advance", "Game Boy Colour", "Game Cube", "Game Gear",
     "Master System", "Super Nintendo", "Mega Drive","N64", "NES", "PS1", "PS2", "Sega Saturn", "SNES", "Xbox"]
