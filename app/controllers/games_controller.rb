013c34fd425fcac38ac91bc694a1d47ef713f862
torentino evans <Torentinoevans@gmail.com>
2024-02-27 16:05:04 +0000

    added destroy and show  methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,33 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @user = Game.where(user: current_user)
+    @game.destroy
+    redirect_to user_path(@user), status: :see_other
+
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+
+
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :console, :genre, :year)
+  end
+
+
+
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :games, only: [:index]
+  resources :games, only: [:index, :destroy, :show]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
