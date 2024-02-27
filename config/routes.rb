3bdcf124ec5acf964f4b0d1cc0e75859bc00ee1b
Mike Tarkleman <tarklemike@outlook.com>
2024-02-27 17:09:43 +0000

    add new and create actions for games controller, and add view for new game

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :games, only: [:index, :destroy, :show]
+  resources :games, only: [:index, :new, :create, :show, :destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
