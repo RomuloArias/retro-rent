44b5e7c41cb5882a12dcb65cb6b1ef25047dffc8
Mike Tarkleman <tarklemike@outlook.com>
2024-02-28 11:08:31 +0000

    fix seed file by adding fantasy genre and making global variables for genres and consoles

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,13 +1,14 @@
 class Game < ApplicationRecord
   belongs_to :user
-
+  GENRES = %w[Action Adventure Arcade Combat Fantasy Platform Puzzle Strategy]
+  CONSOLES = ["Atari", "Dreamcast", "Game Boy", "Game Boy Advance", "Game Boy Colour", "Game Cube", "Game Gear",
+    "Master System", "Super Nintendo", "Mega Drive","N64", "NES", "PS1", "PS2", "Sega Saturn", "SNES", "Xbox"]
   validates :name, presence: true
   validates :console, presence: true, inclusion:
-    { in: ["Atari", "Dreamcast", "Game Boy", "Game Boy Advance", "Game Boy Colour", "Game Cube", "Game Gear",
-           "Master System","Super Nintendo", "Mega Drive","N64", "NES", "PS1", "PS2", "Sega Saturn", "SNES", "Xbox"],
+    { in: CONSOLES,
       message: "%{value} is not a valid console" }
   validates :genre, presence: true, inclusion:
-    { in: %w[Action Adventure Arcade Combat Platform Puzzle Strategy],
+    { in: GENRES,
       message: "%{value} is not a valid genre" }
   validates :price_per_day, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,19 @@ Booking.destroy_all
 Game.destroy_all
 User.destroy_all
 
-user_one = User.create(email: 'cy@pm.me', password: "123456", first_name: "cy", last_name: "porteous")
-user_two = User.create(email: 'lucy@pm.me', password: "123456", first_name: "lucy", last_name: "porter")
-user_three = User.create(email: 'mike@pm.me', password: "123456", first_name: "mike", last_name: "port")
-user_four = User.create(email: 'grace@pm.me', password: "123456", first_name: "grace", last_name: "porton")
+user_one = User.create!(email: 'cy@pm.me', password: "123456", first_name: "cy", last_name: "porteous")
+user_two = User.create!(email: 'lucy@pm.me', password: "123456", first_name: "lucy", last_name: "porter")
+user_three = User.create!(email: 'mike@pm.me', password: "123456", first_name: "mike", last_name: "port")
+user_four = User.create!(email: 'grace@pm.me', password: "123456", first_name: "grace", last_name: "porton")
 
-game_a = Game.create!(name:"Sonic the Hedgehog", console: "Mega Drive", year: 1991, price_per_day: 2, genre: "Action", user: user_one)
-game_b = Game.create(name:"Super Marioworld", console: "Super Nintendo", year: 1991, price_per_day: 2, genre: "Action", user: user_one )
-game_c = Game.create(name:"The Legend of Zelda", console: "Super Nintendo", year: 1992, price_per_day: 2, genre: "Fantasy", user: user_two)
-game_d = Game.create(name:"Wonder Boy", console: "Master System", year: 1986, price_per_day: 4, genre: "Adventure", user: user_two)
-game_e = Game.create(name:"Pac-Man", console: "Atari", year: "1980", price_per_day: 3, genre: "Arcade", user: user_three)
-game_f = Game.create(name:"Street Fighter", console: "Super Nintendo", year: 1994, price_per_day: 3, genre: "Combat", user: user_three)
-game_g = Game.create(name:"Tertris", console: "Super Nintendo", year: 1990, price_per_day: 3, genre: "Puzzle", user: user_four)
-game_h = Game.create(name:"Mortal Combat", console: "Mega Drive", year: 1996, price_per_day: 3, genre: "Combat", user: user_four)
+game_a = Game.create!(name: "Sonic the Hedgehog", console: "Mega Drive", year: 1991, price_per_day: 2, genre: "Action", user: user_one)
+game_b = Game.create!(name: "Super Mario World", console: "Super Nintendo", year: 1991, price_per_day: 2, genre: "Action", user: user_one )
+game_c = Game.create!(name: "The Legend of Zelda", console: "Super Nintendo", year: 1992, price_per_day: 2, genre: "Fantasy", user: user_two)
+game_d = Game.create!(name: "Wonder Boy", console: "Master System", year: 1986, price_per_day: 4, genre: "Adventure", user: user_two)
+game_e = Game.create!(name: "Pac-Man", console: "Atari", year: "1980", price_per_day: 3, genre: "Arcade", user: user_three)
+game_f = Game.create!(name: "Street Fighter", console: "Super Nintendo", year: 1994, price_per_day: 3, genre: "Combat", user: user_three)
+game_g = Game.create!(name: "Tertis", console: "Super Nintendo", year: 1990, price_per_day: 3, genre: "Puzzle", user: user_four)
+game_h = Game.create!(name: "Mortal Combat", console: "Mega Drive", year: 1996, price_per_day: 3, genre: "Combat", user: user_four)
 
 Booking.create!(start_date: '1/3/2024', end_date: '10/3/2024', total_price: 20, confirmed: true, user: user_one, game: game_a)
 Booking.create!(start_date: '1/4/2024', end_date: '10/4/2024', total_price: 30, confirmed: true, user: user_one, game: game_b)
