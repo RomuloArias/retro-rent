4c2e0f2b2b4ac886a5d74fe8b9f3daacdbbdf9bf
Mike Tarkleman <tarklemike@outlook.com>
2024-02-29 13:52:31 +0000

    start adding api to seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_26_141612) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_121249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_26_141612) do
     t.integer "price_per_day"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
