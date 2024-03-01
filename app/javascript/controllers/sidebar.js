fccdd8498006b77c4e4627c59d1a06d6982b3db5
Romulo Arias <raariasalvarez@gmail.com>
2024-03-01 12:49:40 +0000

    fixing cards and navbar

--- app/javascript/controllers/sidebar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuButton = document.querySelector('#hamburger');
-  const sidebar = document.querySelector('#sidebar');
-
-  menuButton.addEventListener('click', () => {
-    sidebar.classList.toggle('sidebar-active');
-  });
-});
