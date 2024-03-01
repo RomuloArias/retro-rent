ac9fec25e6ae63bdaae8968a69ca2af235408e80
Mike Tarkleman <tarklemike@outlook.com>
2024-03-01 11:44:28 +0000

    Fix games#index

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,8 +3,7 @@ class GamesController < ApplicationController
   before_action :set_game, only: [:show, :destroy]
 
   def index
-    @games = current_user.games
-    @games = Game.all.sort_by(&:updated_at).reverse
+    @games = current_user.games.sort_by(&:updated_at).reverse
     if params[:query].present?
       @games = @games.searching(params[:query])
     end
