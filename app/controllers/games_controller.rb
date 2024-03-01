62a0e48730d147cf0daef30de943c9d794d25c62
Mike Tarkleman <tarklemike@outlook.com>
2024-03-01 12:59:17 +0000

    added searching of api when creating a new game, and moved add game button to top of screen

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class GamesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   before_action :set_game, only: [:show, :destroy]
@@ -11,6 +13,22 @@ class GamesController < ApplicationController
 
   def new
     @game = Game.new
+    @game_name = params.dig(:game_search, :name)
+    if @game_name.present?
+      response = URI.open("https://www.giantbomb.com/api/search/?api_key=8bf1a4825f3014203e646d9481076557a0352892&format=json&resources=game&query=#{@game_name}").read
+      @results = JSON.parse(response, symbolize_names: true)[:results]
+      @games = @results
+        .filter { |result| result[:platforms]&.map{ |p| p[:name].downcase }&.intersection(Game::CONSOLES.map(&:downcase))&.any? }
+        .map do |result|
+        Game.new(
+          name: result[:name],
+          console: result[:platforms]&.map { |p| p[:name].downcase }.intersection(Game::CONSOLES.map(&:downcase)).first,
+          description: result[:image][:description],
+          image_url: result[:image][:original_url],
+          year: result[:original_release_date].present? ? Date.parse(result[:original_release_date]).year : result[:expected_release_year]
+        )
+      end
+    end
   end
 
   def create
